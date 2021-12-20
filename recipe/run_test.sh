9f1d5e3a88633d6157f85d62de3553e76bd9a16d
Leo Fang <leof@nvidia.com>
2021-12-20 10:14:49 -0800

    enable building with the public release
    MNT: Re-rendered with conda-build 3.21.6, conda-smithy 3.16.0, and conda-forge-pinning 2021.12.18.06.45.12

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 
+# integraty test
 test -f $PREFIX/include/custatevec.h
 test -f $PREFIX/include/cutensornet.h
 test -f $PREFIX/include/cutensornet/types.h
@@ -8,4 +9,23 @@ test -f $PREFIX/lib/libcustatevec.so
 test -f $PREFIX/lib/libcutensornet.so
 
 # TODO: add dlopen test
-# TODO: add compiling test
+
+# compilation test
+git clone https://github.com/NVIDIA/cuQuantum.git sample_linux/
+cd sample_linux/samples/
+pushd .
+
+cd custatevec
+for f in ./*.cu; do
+    echo $f
+    error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcustatevec $f -o $f.out 2>&1)
+    echo $error_log
+done
+popd
+
+cd cutensornet
+for f in ./*.cu; do
+    echo $f
+    error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcutensornet -lcutensor $f -o $f.out 2>&1)
+    echo $error_log
+done
