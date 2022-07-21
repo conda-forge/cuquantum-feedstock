77aa8129e3634f9b8fc47702a56bb4df93573e14
Leo Fang <leof@nvidia.com>
2022-07-21 16:55:47 -0400

    Update run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -13,8 +13,12 @@ ${GCC} test_load_elf.c -std=c99 -Werror -ldl -o test_load_elf
 ./test_load_elf $PREFIX/lib/libcustatevec.so
 ./test_load_elf $PREFIX/lib/libcutensornet.so
 
+# get the package version (major.minor.patch)
+IFS="." read -a CUQUANTUM_VER <<< $PKG_VERSION 
+CUQUANTUM_VER=v${CUQUANTUM_VER[0]}.${CUQUANTUM_VER[1]}.${CUQUANTUM_VER[2]}
+
 # compilation test
-git clone -b v22.03.0 https://github.com/NVIDIA/cuQuantum.git sample_linux/
+git clone -b $CUQUANTUM_VER https://github.com/NVIDIA/cuQuantum.git sample_linux/
 cd sample_linux/samples/
 pushd .
 
@@ -34,7 +38,9 @@ popd
 
 cd cutensornet
 for f in ./*.cu; do
+    if [[ "$f" == *"mpi"* ]]; then continue; fi  # skip MPI files for simplicity
     echo $f
     error_log=$(nvcc $NVCC_FLAGS --std=c++11 -I$PREFIX/include -L$PREFIX/lib -lcutensornet -lcutensor $f -o $f.out 2>&1)
     echo $error_log
 done
+popd
