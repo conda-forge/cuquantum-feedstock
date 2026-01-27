7ff1a67591ddf1350051c769c2e5f99f91eb19b6
Benedikt Kloss <bkloss@nvidia.com>
2026-01-26 16:30:42 -0800

    Update checksums, versions and add nccl interface for cudensitymat.

--- recipe/cudensitymat/cudm-activate.sh
+++ recipe/cudensitymat/cudm-activate.sh
@@ -4,5 +4,15 @@ if [[ -f "$CONDA_PREFIX/lib/libcudensitymat_distributed_interface_mpi.so" ]]; th
   if [[ -n "${CUDENSITYMAT_COMM_LIB}" ]]; then
     declare -gx CONDA_CUDENSITYMAT_COMM_LIB="${CUDENSITYMAT_COMM_LIB}"
   fi
+  if [[ -n "${CUDENSITYMAT_MPI_COMM_LIB}" ]]; then
+    declare -gx CONDA_CUDENSITYMAT_MPI_COMM_LIB="${CUDENSITYMAT_MPI_COMM_LIB}"
+  fi
+  if [[ -n "${CUDENSITYMAT_NCCL_COMM_LIB}" ]]; then
+    declare -gx CONDA_CUDENSITYMAT_NCCL_COMM_LIB="${CUDENSITYMAT_NCCL_COMM_LIB}"
+  fi
   declare -gx CUDENSITYMAT_COMM_LIB="${CONDA_PREFIX}/lib/libcudensitymat_distributed_interface_mpi.so"
+  declare -gx CUDENSITYMAT_MPI_COMM_LIB="${CONDA_PREFIX}/lib/libcudensitymat_distributed_interface_mpi.so"
+  if [[ -f "$CONDA_PREFIX/lib/libcudensitymat_distributed_interface_nccl.so" ]]; then
+    declare -gx CUDENSITYMAT_NCCL_COMM_LIB="${CONDA_PREFIX}/lib/libcudensitymat_distributed_interface_nccl.so"
+  fi
 fi
